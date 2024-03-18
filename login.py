4b3418faa7e332766b328e3d8f6468ec118b5654
AndreaTomas00 <81638374+AndreaTomas00@users.noreply.github.com>
2024-03-18 06:10:50 +0100

    Update login.py

--- login.py
+++ login.py
@@ -4,15 +4,15 @@ from navigation import make_sidebar
 
 make_sidebar()
 
-st.title("Welcome to Diamond Corp")
+st.title("Dades trasplantament cardíac 2023 - OCATT")
 
-st.write("Please log in to continue (username `test`, password `test`).")
+st.write("Inicia sessió per poder veure la informació).")
 
 username = st.text_input("Username")
 password = st.text_input("Password", type="password")
 
 if st.button("Log in", type="primary"):
-    if username == "test" and password == "test":
+    if username == "cardiac2023" and password == "1234":
         st.session_state.logged_in = True
         st.success("Logged in successfully!")
         sleep(0.5)
